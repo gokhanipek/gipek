e0e952fd5d015cfbde17d93ecabe1939a450b0f7
ipekg <goekhan.ipek@tipico.com>
2022-06-01 02:10:31 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './assets/sass/main.scss';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<header id="header">
+				<h1>Gokhan Ipek</h1>
+				<p>Hi 👋 I am Gokhan, a Frontend Developer based in Karlsruhe, Germany.</p>
+			</header>
+			<footer id="footer">
+				<ul className="icons">
+					<li><a href="https://www.linkedin.com/in/gokhanipek/" target="_blank" rel="noreferrer" className="icon brands fa-linkedin"><span className="label">Linkedin</span></a></li>
+					<li><a href="https://twitter.com/gipek03" target="_blank" rel="noreferrer" className="icon brands fa-twitter"><span className="label">Twitter</span></a></li>
+					<li><a href="https://medium.com/@gokhanipek" target="_blank" rel="noreferrer" className="icon brands fa-medium"><span className="label">Medium</span></a></li>
+					<li><a href="https://github.com/gokhanipek" target="_blank" rel="noreferrer" className="icon brands fa-github"><span className="label">Github</span></a></li>
+					<li><a href="mailto:g.ipek@outlook.com?subject=Hi Gokhan!" target="_blank" rel="noreferrer" className="icon fa-envelope"><span className="label">Email</span></a></li>
+				</ul>
+			</footer>
+		</>
+	);
 }
 
 export default App;
