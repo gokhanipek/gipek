ac397b467c4edff0fadc019214a55292be27233a
ipekg <goekhan.ipek@tipico.com>
2022-06-02 01:55:32 +0200

    a small refactor

---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+export const socialLinks = [
+    {link: 'https://www.linkedin.com/in/gokhanipek/', customClass: 'brands fa-linkedin', text: 'Linkedin'},
+    {link: 'https://twitter.com/gipek03/', customClass: 'brands fa-twitter', text: 'Twitter'},
+    {link: 'https://medium.com/@gokhanipek/', customClass: 'brands fa-medium', text: 'Medium'},
+    {link: 'https://github.com/gokhanipek/', customClass: 'brands fa-github', text: 'Github'},
+    {link: 'mailto:g.ipek@outlook.com?subject=Hi Gokhan!', customClass: 'fa-envelope', text: 'Email'},
+]
